c782ef27242d05ad033cd34339a145ae16943bbf
Patrícia Silva <prrs89@gmail.com>
2020-03-29 22:27:19 +0100

    feat: Add active class to section in view

--- js/app.js
+++ js/app.js
@@ -17,11 +17,9 @@
  * Define Global Variables
  *
  */
-const fragment = document.createDocumentFragment();
+const navList = document.getElementById('navbar__list');
 
-const navList = document.getElementById("navbar__list");
-
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section');
 /**
  * End Global Variables
  * Start Helper Functions
@@ -34,40 +32,53 @@ const sections = document.querySelectorAll("section");
  *
  */
 // build the nav
+const buildNav = () => {
+  const fragment = document.createDocumentFragment();
+
+  sections.forEach(section => {
+    const dataValue = section.dataset.nav;
+    const idValue = section.id;
+    const navItem = document.createElement('li');
+    const navLink = document.createElement('a');
+    navLink.textContent = dataValue;
+
+    navLink.setAttribute('href', `#${idValue}`);
+    navLink.setAttribute('class', 'menu__link');
+    navLink.setAttribute('data-sectionId', `${idValue}`);
+
+    navItem.appendChild(navLink);
+
+    fragment.appendChild(navItem);
+  });
+
+  navList.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
+const setSectionsAsActive = () => {
+  const minBorderPosition = 250;
+  const maxBorderPosition = 1020;
+
+  sections.forEach(section => {
+    // getBoundingClientRect() returns the size of an element and its position relative to the viewport
+    const sectionBottomPosition = section.getBoundingClientRect().bottom;
+
+    if (
+      sectionBottomPosition > minBorderPosition &&
+      sectionBottomPosition < maxBorderPosition
+    ) {
+      section.classList.add('active');
+    } else {
+      section.classList.remove('active');
+    }
+  });
+}
 
 // Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-// Build menu
-sections.forEach(section => {
-  const dataValue = section.dataset.nav;
-  const idValue = section.id;
-  const navItem = document.createElement("li");
-  const navLink = document.createElement("a");
-  navLink.textContent = dataValue;
-
-  navLink.setAttribute("href", `#${idValue}`);
-  navLink.setAttribute("class", "menu__link");
-  navLink.setAttribute("data-sectionId", `${idValue}`);
-
-  navItem.appendChild(navLink);
-
-  fragment.appendChild(navItem);
-});
-
-navList.appendChild(fragment);
-
-// Scroll to section on link click
-navList.addEventListener("click", function(e) {
+const scrollToSection = (e) => {
   e.preventDefault();
 
-  if (e.target.nodeName === "A") {
+  if (e.target.nodeName === 'A') {
     const targetSection = document.getElementById(e.target.dataset.sectionid);
 
     const sectionTopPosition =
@@ -75,25 +86,18 @@ navList.addEventListener("click", function(e) {
 
     window.scrollTo(0, sectionTopPosition);
   }
-});
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+// Build menu
+document.addEventListener('DOMContentLoaded', buildNav);
+
+// Scroll to section on link click
+navList.addEventListener('click', scrollToSection);
 
 // Set sections as active
-// TODO don't remove active from section 1 until it scrolls to another section
-document.addEventListener("scroll", function() {
-  const borderPosition = 250;
-
-  sections.forEach(section => {
-    // getBoundingClientRect() returns the size of an element and its position relative to the viewport
-    const sectionTopPosition = section.getBoundingClientRect().top;
-    const sectionBottomPosition = section.getBoundingClientRect().bottom;
-
-    if (
-      sectionTopPosition <= borderPosition &&
-      sectionBottomPosition > borderPosition
-    ) {
-      section.classList.add("active");
-    } else {
-      section.classList.remove("active");
-    }
-  });
-});
+document.addEventListener('scroll', setSectionsAsActive);
